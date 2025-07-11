4b3cf41fb997901e7d2f8ac4bfbf61bcb7af6d8f
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-11 12:42:44 +0530

    update code

--- src/Components/FeaturedSection.jsx
+++ src/Components/FeaturedSection.jsx
@@ -2,6 +2,9 @@ import { ArrowRight } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "./BlurCircle";
+import MovieCard from "./MovieCard";
+import { dummyShowsData } from "../assets/assets";
+
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
@@ -13,18 +16,29 @@ const FeaturedSection = () => {
         <p className="text-gray-300 font-medium text-lg">New Events</p>
         <button
           onClick={() => navigate("/movies")}
-          className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer">
-        
+          className="group flex items-center gap-2 text-sm text-gray-300 cursor-pointer"
+        >
           View All
           <ArrowRight className="group-hover:translate-x-0.5 transition w-4.5 h-4.5" />
         </button>
       </div>
-      <div></div>
-      <div className='flex justify-center mt-20'>
-        <button onClick={() => { navigate('/movies'); scrollTo(0, 0) }}
-          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">Show More
-        </button>
+      <div className="flex flex-wrap max-sm:justify-center gap-8 mt-8">
+        {dummyShowsData.slice(0, 4).map((show) => (
+          <MovieCard key={show.id} movie={show} />
+        ))}
+      </div>
 
+      <div className="flex justify-center mt-20">
+        <button
+          onClick={() => {
+            navigate("/movies");
+            window.scrollTo(0, 0);
+
+          }}
+          className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer"
+        >
+          Show More
+        </button>
       </div>
     </div>
   );
