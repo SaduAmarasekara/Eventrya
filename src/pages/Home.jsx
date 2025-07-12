cd7005bc8ee85245499d347417e49ef4ca9f03b5
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-12 13:10:29 +0530

    new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../Components/HeroSection'
 import FeaturedSection from '../Components/FeaturedSection'
+import TrailersSection from '../Components/TrailersSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
-      <FeaturedSection/>
+      <FeaturedSection />
+      <TrailersSection />
     </>
   )
 }
