27e48337c7712f38f84d358bf6d81513b6cb4b1b
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-15 09:04:07 +0530

    add this

--- src/Components/TrailersSection.jsx
+++ src/Components/TrailersSection.jsx
@@ -8,7 +8,7 @@ const TrailersSection = () => {
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden">
       <p className="text-gray-300 font-medium text-lg max-w-[960px] mx-auto">
-        Trailers
+        EventHighlights
       </p>
 
       <div className="relative mt-6">
