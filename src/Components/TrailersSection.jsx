647bdbb1f8f36eeb0ff98bf642571dc46d07ae02
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-19 16:36:40 +0530

    complete trailer section

--- src/Components/TrailersSection.jsx
+++ src/Components/TrailersSection.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { dummyTrailers } from "../assets/assets";
 import BlurCircle from "./BlurCircle";
+import { PlayCircleIcon } from "@heroicons/react/24/solid";
+                                
+
 
 const TrailersSection = () => {
   const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
@@ -21,6 +24,26 @@ const TrailersSection = () => {
           width="960"
           height="540"
         />
+      </div>
+      <div className="group grid grid-cols-4 gap-4 md:gap-8 max-w-3xl mx-auto ">
+        {dummyTrailers.map((trailer) => (
+          <div
+            key={trailer.image}
+            className="relative hover:opacity-100 transition duration-300
+ max-md:h-60 cursor-pointer"
+            onClick={() => setCurrentTrailer(trailer)}
+          >
+            <img
+              src={trailer.image}
+              alt="trailer"
+              className="rounded-lg w-full h-full object-cover brightness-75"
+            />
+            <PlayCircleIcon
+              strokeWidth={1.6}
+              className="absolute rounded top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2 -translate-y-1/2"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
