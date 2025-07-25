f706e60c60e49d9f257ff836ddae88171a2d29d4
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-25 16:25:58 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,12 @@ import Favourite from './pages/Favourite'
 import Navbar from './Components/Navbar'
 import { Toaster } from 'react-hot-toast'
 import Footer from './Components/Footer'
+import { Layout } from 'lucide-react'
+import Dashbord from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+
 
 const App = () => {
   const location = useLocation();
@@ -19,12 +25,18 @@ const App = () => {
       <Toaster />
       {!isAdminRoute && <Navbar />}
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/movies/:id' element={<MovieDetails />} />
-        <Route path='/movies/:id/:date' element={<SeatLayout />} />
-        <Route path='/my-bookings' element={<MyBookings />} />
-        <Route path='/favourite' element={<Favourite />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favourite" element={<Favourite />} />
+        <Route path="/admin/*" element={<Layout />} >
+          <Route index element={<Dashbord />} /> {/*same route eke  dekam nisa */}
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+          </Route >
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
