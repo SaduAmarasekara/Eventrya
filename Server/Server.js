fb4eee5134a791cd77748e25d60d817b7db8ae5b
Saduni Amarasekara <saduvindhaya@gmail.com>
2025-07-31 18:00:58 +0530

    first commit

--- Server/Server.js
+++ Server/Server.js
@@ -4,14 +4,12 @@ import "dotenv/config";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js";
-
-
+import { inngest, functions } from "./inngest/index.js"; 
 
 const app = express();
 const port = 300;
 
-await connectDB()
+await connectDB();
 
 // Middleware
 app.use(express.json());
@@ -20,8 +18,7 @@ app.use(clerkMiddleware());
 
 // API Routes
 app.get("/", (req, res) => res.send("Server is Live!"));
-app.use('/api/inngest', serve({ client: inngest, functions }));
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
